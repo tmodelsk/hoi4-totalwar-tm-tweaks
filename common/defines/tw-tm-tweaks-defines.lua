c52ddfc3a08e5422383503a3bd985834532f5030
Tomasz Modelski <Modelski.Tomasz@gmail.com>
2022-06-03 21:13:18 +0200

    Normal diff research speed -10%

--- common/defines/tw-tm-tweaks-defines.lua
+++ common/defines/tw-tm-tweaks-defines.lua
@@ -2,3 +2,6 @@ NDefines.NDiplomacy.VOLUNTEERS_PER_COUNTRY_ARMY = 0.075;
 NDefines.NDiplomacy.VOLUNTEERS_DIVISIONS_REQUIRED = 30;
 --NDefines.NCountry.SPECIAL_FORCES_CAP_BASE = 0.075;
 NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 24;  -- org = 12 (looks like)
+-- Faster compliance growth
+--NDefines.NResistance.COMPLIANCE_GROWTH_BASE = 0.071 -- org 0.071 +20%
+--NDefines.NFOCUS.FOCUS_POINT_DAYS = 9 -- 7
